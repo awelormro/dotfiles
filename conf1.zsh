6f262460eebbb7b2b3138549217581b44edb67d9
Marco Antonio Romero <ssuuppeerr@live.com.mx>
2023-11-27 14:45:44 -0600

    vim 0.01
    added nnn configurations

---
+++ conf1.zsh
@@ -0,0 +1,5 @@
+export NNN_B1="d:$HOME/Documentos;D:$HOME/Descargas/;g:/home/abu/Documentos/gcmfiles"
+export NNN_B2="t:/home/abu/Documentos/tesiscompleta;i:/home/abu/.dotfiles;p:/home/abu/Plantillas"
+export NNN_BMS="$NNN_B1;$NNN_B2"
+export NNN_PLUG='f:finder;o:fzopen;p:mocq;d:diffs;t:nmount;v:preview-tui'
+export NNN_FIFO="/tmp/nnn.fifo"
